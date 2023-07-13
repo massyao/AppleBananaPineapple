70379b970b0eea59b92efd8f68e4c0ce45a98678
massyao <massyao@126.com>
2023-07-13 22:14:56 +1000

    Update how_can_I_get_the_maximum_interests.js

--- AppleBananaPinapple/how_can_I_get_the_maximum_interests.js
+++ AppleBananaPinapple/how_can_I_get_the_maximum_interests.js
@@ -1,4 +1,3 @@
-
 function calc_my_interests(init_saving) {
 	const total = 70
 	const interest_rate = 0.045 / 12
@@ -11,7 +10,8 @@ function calc_my_interests(init_saving) {
 		let interests_one_month = saving * interest_rate
 		interests = interests + interests_one_month
 	}
-	return interests.toFixed(2) - 0
+	const low_rate_interests = 70 * 0.02 / 12 * (35 - time)
+	return (interests + low_rate_interests).toFixed(2) - 0
 }
 
 (() => {
@@ -19,6 +19,6 @@ function calc_my_interests(init_saving) {
 	const result = Array.from(new Array(70), (e, i) => i).map((init_saving_iter) => calc_my_interests(init_saving_iter))
 	const the_max_interest = Math.max(...result)
 	const init_saving = result.indexOf(the_max_interest)
-	console.log('you should put ' , init_saving, ' at first month', ' so in ', (total - init_saving) / 2, ' months you can get the_max_interest ', the_max_interest )
-	console.log('in contrast if you put all the money into saving account, you will get', (total - init_saving) / 2 * 70 * 0.02 / 12)
+	console.log('you should put ' , init_saving, ' at first month', ' so you can get the_max_interest ', the_max_interest )
+	console.log('in contrast if you put all the money into saving account, you will get', 70 * 0.02 / 12 * 35)
 })()
